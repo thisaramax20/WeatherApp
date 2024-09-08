fb9a74d45f0ced6ab552c1ee4e606b9a7d5d2eb4
thisaramax20 <thisarathejana20@gmail.com>
2024-09-08 07:59:44 +0530

    third commit

--- app.js
+++ app.js
@@ -209,3 +209,11 @@ searchCity.addEventListener("keyup", (key) => {
     return;
   }
 });
+
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
